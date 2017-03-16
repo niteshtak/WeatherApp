2e7240b7508c4f8c9aa9feed52ec7602dfa92938
niteshtak <niteshtak@outlook.com>
2017-03-16 16:55:32 +0800

    Added Weather Table View controller

--- WeatherApp/ForecastAPI/WeatherForecastFetcher.swift
+++ WeatherApp/ForecastAPI/WeatherForecastFetcher.swift
@@ -6,4 +6,84 @@
 //  Copyright © 2017 Nitesh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ForecastFetcher: Any {
+
+    func fetchForCoordinatesWithLatitude(_ latitude: Double, longitude: Double) -> (String, [HourWeatherData])? {
+        let timestamp = Int(Date().timeIntervalSince1970) - 10800
+
+        if let json = getJSON("https://api.forecast.io/forecast/c015eee211fa47127a008cbe077a0f17/\(latitude),\(longitude),\(timestamp)?units=si") {
+            let asDictionary = parseJSON(json)
+            let weatherDescription = getWeatherDescription(asDictionary)
+            let hourlyWeatherData = getHourlyWeatherData(asDictionary)
+
+            return (weatherDescription, hourlyWeatherData)
+        }
+
+        else {
+            return nil
+        }
+    }
+
+    fileprivate func getJSON(_ urlToRequest: String) -> Data? {
+        return (try? Data(contentsOf: URL(string: urlToRequest)!))
+    }
+
+    fileprivate func parseJSON(_ inputData: Data) -> Dictionary<String, AnyObject> {
+        do {
+            let weatherData = try JSONSerialization.jsonObject(with: inputData, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            return weatherData as! Dictionary<String, AnyObject>
+        } catch {
+            print("Invalid Selection.")
+        }
+
+        return Dictionary<String, AnyObject>()
+    }
+
+    fileprivate func getWeatherDescription(_ data: Dictionary<String, AnyObject>) -> String {
+        let hourly = data["hourly"] as! Dictionary<String, AnyObject>
+        let description = hourly["summary"] as! String
+        return "\(description)"
+    }
+
+    fileprivate func getHourlyWeatherData(_ data: Dictionary<String, AnyObject>) -> [HourWeatherData] {
+        let hourly = data["hourly"] as! Dictionary<String, AnyObject>
+        let data = hourly["data"] as! [AnyObject]
+        var asHourData: [HourWeatherData] = []
+
+        for forecast in data {
+            if let parsed = sortByHourAndConvertToWeatherData(forecast) {
+                asHourData.append(parsed)
+            }
+        }
+
+        return asHourData
+    }
+
+    fileprivate func sortByHourAndConvertToWeatherData(_ forecast: AnyObject) -> HourWeatherData? {
+
+        let icon = forecast["icon"] as! String
+        let forecastTime = Date(timeIntervalSince1970: TimeInterval(forecast["time"] as! Int))
+        let temperature = forecast["temperature"] as! Double
+        let precipation = forecast["precipIntensity"] as? Double
+
+        return HourWeatherData(icon: icon, time: forecastTime, temperature: temperature, precipation: precipation)
+    }
+}
+
+class HourWeatherData {
+    var icon: String = ""
+    var time: Date
+    var temperature: Double = 0.0
+    var precipation: Double = 0.0
+
+    init(icon: String, time: Date, temperature: Double, precipation: Double?) {
+        self.icon = icon
+        self.time = time
+        self.temperature = temperature
+        if let prec = precipation {
+            self.precipation = prec
+        }
+    }
+}
--- WeatherApp/ViewController/WeatherTableVC.swift
+++ WeatherApp/ViewController/WeatherTableVC.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import UIKit
+
+class WeatherTableVC: UITableViewController {
+
+    var data: [HourWeatherData] = []
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func setTableViewData(_ toSetWith: [HourWeatherData]) {
+        data = toSetWith
+        let table = self.view as! UITableView
+        table.reloadData()
+        //table.transform = CGAffineTransform(rotationAngle: CGFloat(-M_PI * 0.5));
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherTableViewCell
+        let forecast = data[indexPath.row]
+
+        cell.iconImage.image = UIImage(named: forecast.icon)
+        cell.timeLabel.text = getPrintDate(forecast.time as Date)
+        cell.tempLabel.text = String(format: "%.1f", forecast.temperature) + " °C"
+        cell.precipationConstraint.constant = CGFloat(200 * forecast.precipation)
+
+        return cell
+    }
+
+    fileprivate func getPrintDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+
+        let calendar = Calendar.current
+
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+
+        if minute == 0 {
+            return "\(hour):\(minute)\(minute)"
+        }
+        else {
+            return "\(hour):\(minute)"
+        }
+    }
+}
+
+class WeatherTableViewCell: UITableViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var precipationConstraint: NSLayoutConstraint!
+}
