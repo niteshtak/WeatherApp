928483ee292c766ac070d95a0b59008111ff38f7
niteshtak <niteshtak@outlook.com>
2017-03-16 16:52:18 +0800

    Added Weather forecast fetcher.

---
+++ WeatherApp/ForecastAPI/WeatherForecastFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherForecastFetcher.swift
+//  WeatherApp
+//
+//  Created by Nitesh Tak on 16/03/2017.
+//  Copyright © 2017 Nitesh. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/ViewController/WeatherTableVC.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherTableVC.swift
+//  WeatherApp
+//
+//  Created by Nitesh Tak on 16/03/2017.
+//  Copyright © 2017 Nitesh. All rights reserved.
+//
+
+import Foundation
